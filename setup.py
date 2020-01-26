2090427a9bd0bf9ac3c29fd1f52db7ec28aa938c
Torr <torrpolakow@gmail.com>
2020-01-27 00:39:55 +0200

    renamed

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='hammers',
+setup(name='torrs_hammers',
       version='0.1',
       description='useful tools',
       url='http://https://github.com/PTorr/my_tools',
       author='Torr',
       author_email='',
       license='MIT',
-      packages=['hammers'],
+      packages=['torrs_hammers'],
       zip_safe=False)
